97e66d4862996cbfa2a42a91bf8a3358e14d818f
billyjason <byiringirobill@gmailcom>
2019-09-30 00:00:24 +0100

    base

--- powergrab/src/main/java/uk/ac/ed/inf/powergrab/Direction.java
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/Direction.java
@@ -1,5 +1,8 @@
 package uk.ac.ed.inf.powergrab;
 
 public class Direction {
+	public enum direction {
+		
+	};
 
 }
--- powergrab/src/main/java/uk/ac/ed/inf/powergrab/Position.java
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/Position.java
@@ -1,5 +1,19 @@
 package uk.ac.ed.inf.powergrab;
 
 public class Position {
+	public double latitude;
+	public double longitude;
+	public Position(double latitude, double longitude) 
+	{
+		
+	}
+	public Position nextPosition(Direction direction) 
+	{ 
+		return this;
+	}
+	public boolean inPlayArea() 
+	{ 
+		return true;
+	}
 
 }
